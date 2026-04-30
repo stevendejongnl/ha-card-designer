e4e3ebb55a557a814aefcb4515d30c6fa9941d1d
Steven de jong 🍟 <sdejong@cloudsuite.com>
2026-04-30 11:10:29 +0200

    chore(version): bump VERSION to 0.1.2 to bust JS cache for entity/stack editor release

--- custom_components/ha_card_designer/const.py
+++ custom_components/ha_card_designer/const.py
@@ -4,7 +4,7 @@ DOMAIN = "ha_card_designer"
 NAME = "Card Designer"
 PANEL_URL = "/ha_card_designer_panel.js"
 PANEL_ELEMENT = "ha-card-designer-panel"
-VERSION = "0.1.1"
+VERSION = "0.1.2"
 SIDEBAR_ICON = "mdi:palette"
 
 LOGGER = logging.getLogger(__name__)
